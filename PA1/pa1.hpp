39ea74e194d8e13b4c3a9f330c892d1f371405cd
Nicholas Ionata <n.ionata129@gmail.com>
2018-09-20 14:10:53 -0400

    Initial commit

---
+++ PA1/pa1.hpp
@@ -0,0 +1,28 @@
+//
+//  pa1.hpp
+//  
+//
+//  Created by Nicholas Ionata on 9/20/18.
+//
+
+#ifndef pa1_hpp
+#define pa1_hpp
+
+#include <stdio.h>
+#include <string>
+
+class LinkedList() {
+	private:
+		Node* head;
+		Node* tail;
+	public:
+		LinkedList();
+		void insertEnd(std::String value);
+		void insert(std::String value);
+}
+
+class Node() {
+	
+}
+
+#endif /* pa1_hpp */
