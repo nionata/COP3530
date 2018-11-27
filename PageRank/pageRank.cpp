5359d83ee57effa36f265c1146ac5c7c90eccd27
Nicholas Ionata <n.ionata129@gmail.com>
2018-11-27 14:13:24 -0500

    Setup new page rank project

---
+++ PageRank/pageRank.cpp
@@ -0,0 +1,15 @@
+//
+//  pageRank.cpp
+//  
+//
+//  Created by Nicholas Ionata on 11/27/18.
+//
+
+#include <string>
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	std::cout << "Hello";
+}
